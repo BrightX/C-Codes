a0b19fddcbc7b8215a13254431b8e225dc122cdf
BrightX <47632184+BrightX@users.noreply.github.com>
2019-06-01 10:57:03 +0800

    Update 斐波那契数列.c

--- 经典算法/斐波那契数列.c
+++ 经典算法/斐波那契数列.c
@@ -2,15 +2,16 @@
 int main()
 {
 	int a = 1, b = 1, c, n = 3;
-	printf("%-5d%-5d", a, b);
+	printf("%d %d ", a, b);
 	while(n <= 20)
 	{
 		c = a + b;
 		a = b;
 		b = c;
-		printf("%-5d", c);
+		printf("%d", c);
 		if (n%5 == 0)
 		   printf("\n");
+		else printf(" ");
 		n++;
 	}
 	return 0;
