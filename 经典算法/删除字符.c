e5ef931273969c9eecb7a93142b908295da1827e
BrightX <47632184+BrightX@users.noreply.github.com>
2020-11-23 11:46:36 +0800

    Update 删除字符.c

--- 经典算法/删除字符.c
+++ 经典算法/删除字符.c
@@ -1,21 +1,16 @@
 #include<stdio.h>
-void delchar(char a[], char c)
-{
-	char *i, *j;
-	for (i = a, j = a; *i != '\0'; i++)
-	{
-		if (*i != c)
-		{
+void delchar(char a[], char c) {
+	char* i, * j;
+	for (i = a, j = a; *i != '\0'; i++) {
+		if (*i != c) {
 			*j = *i;
 			j++;
 		}
+		*i = '\0';
 	}
-	for (; *j != '\0'; j++)
-	    *j = '\0';
 }
 
-int main()
-{
+int main() {
 	char a[] = "China is wonderful";
 	delchar(a, 'w');
 	puts(a);
